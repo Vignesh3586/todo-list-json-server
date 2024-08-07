ecb03e59392c68f93cab4331d86dcc7b51883b0b
Vignesh3586 <vigneshr3586@gmail.com>
2024-08-07 20:39:20 +0530

    update

--- api/server.js
+++ api/server.js
@@ -3,15 +3,7 @@ const jsonServer = require('json-server')
 const cors = require('cors');
 const path = require('path');
 
-const server = jsonServer.create()
-
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const server = jsonServer.create();
 
 // Comment out to allow write operations
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
@@ -31,10 +23,9 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
-
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
 // Export the Server API
 module.exports = server
